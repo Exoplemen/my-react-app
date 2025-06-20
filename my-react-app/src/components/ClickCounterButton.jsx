1c587542923569c163514867c5fbe1e3fdaa8935
Pelmen <suetinleva1@gmail.com>
2025-06-20 18:07:04 +0300

    Revert "8.04"

--- my-react-app/src/components/ClickCounterButton.jsx
+++
@@ -1,8 +0,0 @@
-function ClickCounterButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Кликов: {count}
-    </button>
-  );
-}
-export default ClickCounterButton;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserInfo from'./components/UserInfo'
-import Product from'./components/UserList'
-import WeatherCard from'./components/WeatherCard'
-import Product from'./components/Product'
-import ClickCounterButton from'./components/ClickCounterButton'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <UserInfo />
-    <UserList />
-    <Product />
-    <WeatherCard />
-    <ClickCounterButton />
   </React.StrictMode>
 );
